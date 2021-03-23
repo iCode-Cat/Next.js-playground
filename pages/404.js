233d928f5ed3613e69fa48cf27103689698572b2
iCode-Cat <devranboyac8@gmail.com>
2021-03-23 12:56:27 +0200

    404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+const Notfound = () => {
+    return ( 
+        <div className="">
+            <h1>OPPS...</h1>
+            <p>This page has been not found!</p>
+        </div>
+     );
+}
+ 
+export default Notfound;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link';
 import Footer from '../componets/Footer'
 import Navbar from '../componets/Navbar'
 import styles from '../styles/Home.module.css'
@@ -7,11 +8,11 @@ export default function Home() {
   return (
     
     <div className="">
-    <Navbar/>
-      <h1>HELLO WORLD!</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis error libero, aliquid distinctio veritatis accusamus perspiciatis? Earum quaerat, totam ducimus, dignissimos maxime quod nemo commodi rem accusantium ad ipsam sunt.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis error libero, aliquid distinctio veritatis accusamus perspiciatis? Earum quaerat, totam ducimus, dignissimos maxime quod nemo commodi rem accusantium ad ipsam sunt.</p>
-      <Footer/>
+ 
+      <h1 className={styles.title}>HELLO WORLD!</h1>
+      <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis error libero, aliquid distinctio veritatis accusamus perspiciatis? Earum quaerat, totam ducimus, dignissimos maxime quod nemo commodi rem accusantium ad ipsam sunt.</p>
+      <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis error libero, aliquid distinctio veritatis accusamus perspiciatis? Earum quaerat, totam ducimus, dignissimos maxime quod nemo commodi rem accusantium ad ipsam sunt.</p>
+      <Link href='/ninjas'><a className={styles.btn}>See Ninja Listing</a></Link>
     </div>
   )
 }
