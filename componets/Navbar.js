54d2c6383434e87a5101e2692cd04eeca38dc62e
iCode-Cat <devranboyac8@gmail.com>
2021-03-23 17:59:15 +0200

    End of the lesson

--- componets/Navbar.js
+++ componets/Navbar.js
@@ -11,11 +11,11 @@ const Navbar = () => {
 
     <nav>
         <div className="logo">
-            <Image src="/me.jpg" width={'100%'} height={'100%'} />
+            <h1>USERS</h1>
         </div>
        <Link href='/'><a>Home</a></Link>
        <Link href='/about'><a>About</a></Link>
-       <Link href='/ninjas'><a>Ninja Listing</a></Link>
+       <Link href='/ninjas'><a>User Listing</a></Link>
     </nav> 
     </>
     );
--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <h1 className={styles.title}>HELLO WORLD!</h1>
       <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis error libero, aliquid distinctio veritatis accusamus perspiciatis? Earum quaerat, totam ducimus, dignissimos maxime quod nemo commodi rem accusantium ad ipsam sunt.</p>
       <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis error libero, aliquid distinctio veritatis accusamus perspiciatis? Earum quaerat, totam ducimus, dignissimos maxime quod nemo commodi rem accusantium ad ipsam sunt.</p>
-      <Link href='/ninjas'><a className={styles.btn}>See Ninja Listing</a></Link>
+      <Link href='/ninjas'><a className={styles.btn}>See User Listing</a></Link>
     </div>
   )
 }
