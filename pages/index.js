955c5e4b3b61f458a7814fbe0751823a3f7a5bf4
iCode-Cat <devranboyac8@gmail.com>
2021-03-24 22:23:36 +0200

    strapi updated with request time rendered

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,32 @@ import Link from 'next/link';
 import Footer from '../componets/Footer'
 import Navbar from '../componets/Navbar'
 import styles from '../styles/Home.module.css'
+import {useEffect, useState} from 'react';
 
-export const getStaticProps = async () => {
+export default function Home() {
 
-  const get = await fetch('http://localhost:1337/titles')
-  const data = await get.json();
-  console.log(data);
-  return {
-    props : {data}
-  }
+  const [data, setData] = useState([]);
+  
 
-}
+  useEffect(()=>{
 
-export default function Home({data}) {
+  
+    const strapis = async () => {
+
+      const get = await fetch('http://localhost:1337/titles')
+      const data = await get.json();
+      setData(data)
+    }
+     strapis()
+    console.log(data);
+  }, [])
+
+ 
 
   return (
     
     <div className="">
-      {data.map((x) => (<div key={x.id} className="">{x.Title}</div>))}
+      {data.map(data => (<h1>{data.Title}</h1>))}
       <h1 className={styles.title}>HELLO WORLD!</h1>
       <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis error libero, aliquid distinctio veritatis accusamus perspiciatis? Earum quaerat, totam ducimus, dignissimos maxime quod nemo commodi rem accusantium ad ipsam sunt.</p>
       <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis error libero, aliquid distinctio veritatis accusamus perspiciatis? Earum quaerat, totam ducimus, dignissimos maxime quod nemo commodi rem accusantium ad ipsam sunt.</p>
