c9235de08e49b5aab1d57c43cd68968b83093232
iCode-Cat <devranboyac8@gmail.com>
2021-03-24 22:04:07 +0200

    Strapi added

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,23 @@ import Footer from '../componets/Footer'
 import Navbar from '../componets/Navbar'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export const getStaticProps = async () => {
+
+  const get = await fetch('http://localhost:1337/titles')
+  const data = await get.json();
+  console.log(data);
+  return {
+    props : {data}
+  }
+
+}
+
+export default function Home({data}) {
+
   return (
     
     <div className="">
- 
+      {data.map((x) => (<div key={x.id} className="">{x.Title}</div>))}
       <h1 className={styles.title}>HELLO WORLD!</h1>
       <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis error libero, aliquid distinctio veritatis accusamus perspiciatis? Earum quaerat, totam ducimus, dignissimos maxime quod nemo commodi rem accusantium ad ipsam sunt.</p>
       <p className={styles.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis error libero, aliquid distinctio veritatis accusamus perspiciatis? Earum quaerat, totam ducimus, dignissimos maxime quod nemo commodi rem accusantium ad ipsam sunt.</p>
